b72382286ca00fd73257d0f3afb5b07fc3959099
craynex <cris2007.martin@gmail.com>
2023-12-20 14:24:45 +0100

    v1.1

--- Programm.js
+++ Programm.js
@@ -5,6 +5,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const jwt = require('jsonwebtoken') 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger_output.json'); 
 
 
 const app = express();
@@ -13,13 +15,132 @@ const port = 3000;
 app.use(bodyParser.json());
 
 let tasks = [
-      {id: 1, title: 'Task 1'},
-      {id: 2, title: 'Task 2'},
+{     
+      id: 1, 
+      AusgeführterTask: 'Kaffemaschine Einschalten',
+      Ort: 'Küche'
+},
+{     
+      id: 2,
+      AusgeführterTask: 'TV einschalten',
+      Ort: 'Wohnzimmer'
+},
 ];
 
-const secretKey = 'Halloballo123'
 
-app.get('/tasks', (req, res) => {
-      res.status(200)
+// Authentifizierungsschlüssel
+const Key = 'Halloballo123'
+
+// Generiert mit ChatGPT von hier
+// Speichere die ungültigen Tokens (eine Art Blacklist)
+const invalidTokens = new Set();
+
+// Middleware für Authentifizierung
+const authenticate = (req, res, next) => {
+  if (req.path === '/login') {
+    // Für /login-Endpunkt keine Authentifizierung erforderlich
+    next();
+  } else {
+    const token = req.headers.authorization;
+
+    if (token) {
+      const tokenValue = token.split(' ')[1];
+      
+      // Überprüfe, ob das Token auf der Blacklist ist
+      if (invalidTokens.has(tokenValue)) {
+        res.status(401).json({ error: 'Invalid token' });
+      } else {
+        jwt.verify(tokenValue, Key, (err, decoded) => {
+          if (err) {
+            res.status(401).json({ error: 'Invalid token' });
+          } else {
+            req.user = decoded.user;
+            next();
+          }
+        });
+      }
+    } else {
+      res.status(403).json({ error: 'Token not provided' });
+    }
+  }
+};
+
+// Route für den DELETE /logout-Endpunkt
+app.delete('/logout', authenticate, (req, res) => {
+  // Markiere das Token als ungültig, um es zu blockieren
+  const tokenValue = req.headers.authorization.split(' ')[1];
+  invalidTokens.add(tokenValue);
+
+  res.status(204).json({message: 'Logout successful'}); // Erfolgreicher Logout mit Statuscode 204
+});
+
+// Generiert mit ChatGPT bis hier
+
+app.get('/tasks', authenticate, (req, res) => {
+      res.status(200).json(tasks);
 })
 
+app.post('/tasks', authenticate, (req, res) => {
+      // Korrigiert mit ChatGPT von hier
+      const newtask = req.body;
+      tasks.push(newtask);
+      res.status(201).json(newtask);
+      // Korrigiert mit ChatGPT bis hier
+});
+
+app.get('/tasks/:id', authenticate, (req, res) => {
+      const taskId = parseInt(req.params.id);
+      const task = tasks.find(t => t.id === taskId);
+
+      if(task) {
+            res.status(200).json(task);
+      } else {
+            res.status(404).json({error: '404 Not found'});
+      }
+});
+
+app.put('/tasks/:id', authenticate, (req, res) => {
+      const taskId = parseInt(req.params.id);
+      const updatedtask = req.body;
+      const index = tasks.findIndex(t => t.id === taskId);
+
+      if(index !== -1) {
+            tasks[index] = updatedtask;
+            res.status(200).json({message: 'File has been edited'})
+      } else {
+            res.status(404).json({error: '404 Not found'})
+      }
+});
+
+app.delete('/tasks/:id', authenticate, (req, res) => {
+      const taskId = parseInt(req.params.id);
+      const index = tasks.findIndex(t => t.id === taskId)
+      // Korrigiert mit ChatGPT von hier
+      if (index !== -1) {
+            const deletedtask = tasks.splice(index, 1)[0];
+            res.status(200).json({ message: 'task deleted successfully' });
+          } else {
+            res.status(404).json({ error: 'task not found' });
+          }
+        });
+      // Korrigiert mit ChatGPT bis hier
+
+app.post('/login', (req, res) => {
+      const{email, password} = req.body;
+
+      if(email === 'cristian.martin@zli.com' && password === 'Halloballo123') {
+      const token = jwt.sign({user: email}, Key);
+      res.status(200).json({token});
+      } else {
+            res.status(401).json({error: 'Invalid User or Password'}) // Google gefragt welcher Statuscode richtig ist
+      }
+});
+
+app.use('/swagger-ui/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.listen(port, () => {
+      console.log(`Server listening at http://localhost:${port}`);
+});
+// Generiert mit ChatGPT bis hier
+
+// Verwenden von Swagger UI Middleware
--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-Hallo
+# Task Endpnkte auf Local Host
+
+Das Projekt macht einen Localhost auf port 3000 auf dem mehrere Endpunkte existieren die nur Zugänglich sind mit einer Authentifizierung mit Bearer Token. Den Token kann erhalten werden durch den Endpunkt /Login und dann mit der richtigen Email und Passwort.
+
+## Inhalt
+
+- [Installation](#installation)
+- [Verwendung](#verwendung)
+- [API-Dokumentation](#api-dokumentation)
+- [Beitrag](#beitrag)
+- [Lizenz](#lizenz)
+
+## Installation
+
+Beschreibe hier, wie Entwickler dein Projekt lokal installieren können. 
+
+```bash
+# Beispiel für die Installation von Abhängigkeiten
+npm install
+```
+
+## Verwendung
+
+Erkläre, wie Entwickler dein Projekt verwenden können.
+
+```bash
+# Beispiel für die Ausführung deiner Anwendung
+npm start
+```
+
+## API-Dokumentation
+
+Verweise hier auf die generierte API-Dokumentation, wenn vorhanden.
+
+```bash
+# Beispiel für die Generierung der API-Dokumentation
+npm run docs
+```
+
+## Beitrag
+
+Erkläre, wie andere Entwickler zu deinem Projekt beitragen können. Dies könnte Links zu Beitragshinweisen, Richtlinien für Pull-Anfragen usw. umfassen.
+
+## Lizenz
+
+Gib die Lizenzinformationen für dein Projekt an. Zum Beispiel:
+
+Dieses Projekt ist unter der [MIT Lizenz](LICENSE) lizenziert.
+
+```
+
+Ersetze "Project Name" und die Abschnitte mit den relevanten Informationen für dein Projekt. Du kannst auch spezifische Anweisungen für die Installation, Verwendung und den Beitrag in deinem README.md hinzufügen.
